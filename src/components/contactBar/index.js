a5093df5c875ef364cb3d00293b3190baf351a8e
kula <kula124@gmail.com>
2020-08-18 02:58:20 +0200

    project clean up

---
+++ src/components/contactBar/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ContactBar = () => (
+    <main>
+        <span>example@email.com</span>
+        <span>+0385911122334</span>
+    </main>
+)
+
+export default ContactBar
