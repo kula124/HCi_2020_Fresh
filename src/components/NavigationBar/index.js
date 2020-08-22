2df7a35fa24212138d96c605bf30d273a6050179
kula <kula124@gmail.com>
2020-08-22 23:45:35 +0200

    refactor to use props for .active

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -4,9 +4,9 @@ import styles from './style.module.css'
 
 const navTabs = ['Home', 'Accommodation', 'Photo Gallery', 'Contact']
 
-const NavigationBar = () => (
+const NavigationBar = ({ activeTab }) => (
     <nav className={styles.navigationBar}>
-        {navTabs.map(tab => <li className={tab === 'Home' ? styles.active : ''}>
+        {navTabs.map(tab => <li className={tab=== activeTab ? styles.active : ''}>
             {tab}</li>
         )}
     </nav>
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import NavigationHeader from '../components/NavigationHeader'
 const IndexPage = () => (
   <main>
     <ContactBar />
-    <NavigationHeader />
+    <NavigationHeader activeTab = 'Home' />
   </main>
 )
 
