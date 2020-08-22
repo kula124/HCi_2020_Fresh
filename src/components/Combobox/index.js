a8cc12205b6a7d5350fe8b2ceeac21093c395f92
kula <kula124@gmail.com>
2020-08-22 22:25:06 +0200

    adding navigation

---
+++ src/components/Combobox/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import styles from './style.module.css' 
+
+const Combobox = () => <section></section>
+
+export default Combobox
--- src/components/NavigationHeader/index.js
+++ src/components/NavigationHeader/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 import Logo from '../Logo'
+import Navigation from '../Navigation'
 
 import styles from './style.module.css'
 
 const NavigationHeader = () => (
     <section className={styles.navigationHeader}>
+        <Logo />
+        <Navigation />
     </section>
 )
 
