a99f854a580261e591c518bbbb7c41264a227082
kula <kula124@gmail.com>
2020-08-22 18:03:48 +0200

    adding NavigationHeader

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from "react"
+
 import ContactBar from '../components/contactBar'
+import NavigationHeader from '../components/NavigationHeader'
 
 const IndexPage = () => (
   <main>
     <ContactBar />
+    <NavigationHeader />
   </main>
 )
 
