65e237f349e1f60d7dc9052e4a14dca01ec82349
kula <kula124@gmail.com>
2020-08-22 22:25:06 +0200

    adding navigation

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import NavigationBar from '../NavigationBar'
+import Combobox from '../Combobox'
+import styles from './style.module.css'
+
+const Navigation = () => (
+    <section>
+        <NavigationBar />
+        <Combobox />
+    </section>
+)
+
+export default Navigation
