1831313f54bebf5f8516fb900f084e55530a4712
kula <kula124@gmail.com>
2020-08-23 16:21:24 +0200

    adding Combobox

--- src/components/Combobox/index.js
+++ src/components/Combobox/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 import styles from './style.module.css' 
 
-const Combobox = () => <section></section>
+const Combobox = () => <span className={styles.combobox}>English</span>
 
 export default Combobox
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -5,7 +5,7 @@ import Combobox from '../Combobox'
 import styles from './style.module.css'
 
 const Navigation = ({ activeTab }) => (
-    <section>
+    <section className={styles.navigation}>
         <NavigationBar activeTab={activeTab} />
         <Combobox />
     </section>
