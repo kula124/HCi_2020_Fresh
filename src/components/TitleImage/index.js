203cfd614e66241a44c5f371a03dad633e0c837f
kula <kula124@gmail.com>
2020-08-24 22:24:52 +0200

    Creating TitlePage

---
+++ src/components/TitleImage/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import styles from './style.module.css'
+
+const TitleImage = () => (
+    <section className={styles.titleImage}>
+    </section>
+)
+
+export default TitleImage
