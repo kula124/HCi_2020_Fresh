1128d3b49a1c45e98d88ec75f9cea3f599721e09
kula <kula124@gmail.com>
2020-08-24 21:46:01 +0200

    Creating Modules

--- src/modules/NavigationHeader/index.js
+++ src/modules/NavigationHeader/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-import Logo from '../Logo'
-import Navigation from '../Navigation'
-import ContactBar from '../contactBar'
+import Logo from '../../components/Logo'
+import Navigation from '../../components/Navigation'
+import ContactBar from '../../components/contactBar'
 
 import styles from './style.module.css'
 
