e362f89589eec3d8442f06332ab46654aace7eaf
kula <kula124@gmail.com>
2020-08-24 19:39:16 +0200

    Adding ContactBar to NavigationHeader

--- src/components/NavigationHeader/index.js
+++ src/components/NavigationHeader/index.js
@@ -2,14 +2,18 @@ import React from 'react'
 
 import Logo from '../Logo'
 import Navigation from '../Navigation'
+import ContactBar from '../contactBar'
 
 import styles from './style.module.css'
 
 const NavigationHeader = ({activeTab}) => (
-    <section className={styles.navigationHeader}>
-        <Logo />
-        <Navigation activeTab={activeTab} />
-    </section>
+    <>
+        <ContactBar />
+        <section className={styles.navigationHeader}>
+            <Logo />
+            <Navigation activeTab={activeTab} />
+        </section>
+    </>
 )
 
 export default NavigationHeader
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import React from "react"
 
-import ContactBar from '../components/contactBar'
 import NavigationHeader from '../components/NavigationHeader'
 import Footer from '../components/Footer'
 
 const IndexPage = () => (
   <main>
-    <ContactBar />
     <NavigationHeader activeTab = 'Home' />
     <Footer />
   </main>
