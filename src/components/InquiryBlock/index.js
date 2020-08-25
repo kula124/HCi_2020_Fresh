8eea05358d680a2b2af7bfc28301da8b747f4c21
kula <kula124@gmail.com>
2020-08-25 15:39:24 +0200

    Creating InquiryBlock

--- src/components/InquiryBlock/index.js
+++ src/components/InquiryBlock/index.js
@@ -4,6 +4,21 @@ import styles from './style.module.css'
 
 const InquiryBlock = () => (
     <section className={styles.inquiryBlock}>
+        <div className={styles.dateBoxContainer}>
+            <span>Arrival date</span>
+            <div className={styles.dateBox}>
+                <img src="https://github.com/n00ne1mportant/PublicFilesRepo/blob/master/appvd-calendar-2.png?raw=true" />
+                <span>{`${(new Date()).getDate()}.${(new Date()).getMonth() + 1}.${(new Date()).getFullYear()}`}</span>
+            </div>
+        </div>
+        <div className={styles.dateBoxContainer}>
+            <span>Departure date</span>
+            <div className={styles.dateBox}>
+                <img src="https://github.com/n00ne1mportant/PublicFilesRepo/blob/master/appvd-calendar-2.png?raw=true" />
+                <span>{`${(new Date()).getDate() + 1}.${(new Date()).getMonth() + 1}.${(new Date()).getFullYear()}`}</span>
+            </div>
+        </div>
+        <button>Send inquiry</button>
     </section>
 )
 
