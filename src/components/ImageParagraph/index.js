ecd56233ebd56dfad918236b79ba8f205f317646
kula <kula124@gmail.com>
2020-08-25 19:17:13 +0200

    Creating PageContent

---
+++ src/components/ImageParagraph/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './style.module.css'
+
+const ImageParagraph = () => <section className={styles.imageParagraph}>
+
+</section> 
+
+export default ImageParagraph
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React from "react"
 
 import HeaderFooterLayout from "../layouts/headerFooter"
 import TitlePage from '../modules/TitlePage'
+import PageContent from "../modules/PageContent"
 
 const IndexPage = () => (
   <HeaderFooterLayout activeTab="Home">
     <TitlePage />
+    <PageContent />
   </HeaderFooterLayout>
 )
 
