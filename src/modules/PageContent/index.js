e674e431c79ce0acc387819a68843de9276dd0cd
kula <kula124@gmail.com>
2020-08-25 19:17:13 +0200

    Creating PageContent

---
+++ src/modules/PageContent/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './style.module.css'
+import ImageParagraph from '../../components/ImageParagraph'
+
+const PageContent = () => <section className={styles.pageContent}>
+    <ImageParagraph />
+    <ImageParagraph />
+</section> 
+
+export default PageContent
