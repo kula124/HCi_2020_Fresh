260d7d07284b7d01f720ceefbc9d278deba09cb3
kula <kula124@gmail.com>
2020-09-02 19:26:52 +0200

    rework: adding FontAwsome

--- src/components/Combobox/index.js
+++ src/components/Combobox/index.js
@@ -1,8 +1,10 @@
 import React, {useState} from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
 
 import { languages } from '../../constants/const'
 import ComboboxItem from './ComboboxItem'
-import styles from './style.module.css' 
+import styles from './style.module.css'
 
 const Combobox = () => {
     const [selectedItem, setSelectedItem] = useState(languages.find(el => el.name === "eng"))
@@ -17,6 +19,7 @@ const Combobox = () => {
                     />)}
                 </ul>
             }
+            <FontAwesomeIcon icon={faChevronDown} size="0.2x" style={{paddingRight: "12px"}} />
             {selectedItem.language}
         </div>
     )
--- src/components/contactBar/index.js
+++ src/components/contactBar/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
+
 import styles from './style.module.css'
 
 const ContactBar = () => (
     <main className={styles.container}>
         <span>example@email.com</span>
+        <FontAwesomeIcon icon={faEnvelope} color="white" />
         <span>+0385911122334</span>
+        <FontAwesomeIcon icon={faPhone} color="white"/>
     </main>
 )
 
