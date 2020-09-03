6c833b523946c72d9b06e6301dac06ffd376f627
kula <kula124@gmail.com>
2020-09-03 16:15:25 +0200

    Creating acctual Combobox

--- src/components/Combobox/index.js
+++ src/components/Combobox/index.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 
+import { languages } from '../../constants/const'
+import ComboboxItem from './ComboboxItem'
 import styles from './style.module.css' 
 
-const Combobox = () => <span className={styles.combobox}>English</span>
+const Combobox = () => {
+    const [selectedItem, setSelectedItem] = useState(languages.find(el => el.name === "eng"))
+    const [open, setOpen] = useState(false)
+    return (
+        <div className={styles.combobox} onClick={() => setOpen(!open) }>
+            {<ul style={{display: open ? 'flex' : 'none'}}>
+                    {languages.map(el => 
+                    <ComboboxItem {...el} 
+                      setSelected={setSelectedItem}
+                      selected={el.name === selectedItem.name}
+                    />)}
+                </ul>
+            }
+            {selectedItem.language}
+        </div>
+    )
+}
 
 export default Combobox
--- src/constants/const.js
+++ src/constants/const.js
@@ -1,1 +1,19 @@
 export const navs = ['Home', 'Accommodation', 'Photo Gallery', 'Contact']
+
+export const languages = [
+    {
+        language: "English",
+        name: "eng",
+        flag: "http://villaolivaverde.com/static/template_shared/img/en.svg"
+    },
+    {
+        language: "Italiano",
+        name: "it",
+        flag: "http://villaolivaverde.com/static/template_shared/img/it.svg"
+    },
+    {
+        language: "Deutsche",
+        name: "de",
+        flag: "http://villaolivaverde.com/static/template_shared/img/de.svg"
+    },
+]
