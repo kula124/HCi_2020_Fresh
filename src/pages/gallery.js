da66d77612d887fc6fc1ed3552b61c81e02754fd
kula <kula124@gmail.com>
2020-09-09 21:25:02 +0200

    adding PhotoGallery page

---
+++ src/pages/gallery.js
@@ -0,0 +1,11 @@
+import React from "react"
+import HeaderFooterLayout from "../layouts/headerFooter"
+import SeparatorBar from '../modules/SeparatorBar'
+
+const PhotoGallery = () => (
+    <HeaderFooterLayout activeTab="Photo Gallery">
+        <SeparatorBar text="Photo Gallery"/>
+    </HeaderFooterLayout>
+)
+
+export default PhotoGallery
