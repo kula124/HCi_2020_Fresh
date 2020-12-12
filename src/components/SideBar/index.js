360d6ab5ec91239fe8cb5a0e534a2bd3a0c7f38e
kula <kula124@gmail.com>
2020-12-12 18:36:03 +0100

    Adding SideBar

---
+++ src/components/SideBar/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import styles from './style.module.css'
+
+const SideBar = () => (
+  <nav className={styles.container}>
+  </nav>
+  )
+
+export default SideBar
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 
 import Header from '../components/Header'
+import SideBar from "../components/SideBar"
 
 const IndexPage = () => (
   <main>
     <Header />
+    <SideBar/>
   </main>
 )
 
